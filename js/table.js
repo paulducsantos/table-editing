8af173bb6c263f1c734dff59c8ae8c21d8a60cf4
Paul Santos <paulmichael.a.santos@gmail.com>
2015-11-14 23:41:50 -0500

    Working row counter

--- js/table.js
+++ js/table.js
@@ -1,1 +1,22 @@
-alert("hello");
+var deleteButtons = document.getElementsByClassName("delete-row");
+var addButton = document.getElementById("add-row");
+
+for(var i = 0; i < deleteButtons.length; i++) {
+  deleteButtons[i].addEventListener("click", function(){
+    console.log(deleteButtons[i], i);
+    var row =  this.parentNode.parentNode;
+    row.parentNode.removeChild(row)
+  });  
+}
+
+function rowCounter () {
+  var rows = document.getElementById("contacts").getElementsByTagName("tbody")[0].getElementsByTagName("tr").length;
+  rows -= 1;
+  return rows
+}
+
+addButton.addEventListener("click", function(){
+  rowCount = rowCounter();
+  console.log(rowCount)
+})
+
