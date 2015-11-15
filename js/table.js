f50553675ee8b4a03d0fa5b85f21587413ce46e5
Paul Santos <paulmichael.a.santos@gmail.com>
2015-11-15 02:11:16 -0500

    Created working add row button

--- js/table.js
+++ js/table.js
@@ -1,5 +1,11 @@
 var deleteButtons = document.getElementsByClassName("delete-row");
 var addButton = document.getElementById("add-row");
+var rowCount;
+var contactTable = document.getElementById("contacts");
+var tableBody = document.getElementById("table-body");
+var deleteButtonLastColumn = document.createElement("button");
+deleteButtonLastColumn.innerHTML = "X";
+deleteButtonLastColumn.setAttribute("class", "btn btn-default danger delete-row");
 
 for(var i = 0; i < deleteButtons.length; i++) {
   deleteButtons[i].addEventListener("click", function(){
@@ -11,12 +17,28 @@ for(var i = 0; i < deleteButtons.length; i++) {
 
 function rowCounter () {
   var rows = document.getElementById("contacts").getElementsByTagName("tbody")[0].getElementsByTagName("tr").length;
-  rows -= 1;
+  rows = (rows - 1)*2;
   return rows
 }
 
-addButton.addEventListener("click", function(){
+addButton.addEventListener("click", function () {
   rowCount = rowCounter();
-  console.log(rowCount)
-})
+  var newRow = document.createElement("tr");
+  var firstName = document.createTextNode(document.getElementById("first-name").value);
+  var lastName = document.createTextNode(document.getElementById("last-name").value);
+  var email = document.createTextNode(document.getElementById("email-field").value);
+  var rowElements = [firstName, lastName, email, deleteButtonLastColumn];
+  for (i = 0; i < 4; i++) {
+    var newTd = document.createElement("td");
+    var tableData = rowElements[i];
+    newTd.appendChild(tableData);
+    newRow.appendChild(newTd);
+  };
+
+  tableBody.insertBefore(newRow, tableBody.childNodes[rowCount]);
+  rowCounter();
+});
+
+
+
 
